7e67e66d6469681e7f633d2d5067dacc0803d409
jmrshl <jamesmrshl@gmail.com>
2018-10-30 21:46:45 -0700

    update

--- blockstacking.py
+++ blockstacking.py
@@ -12,7 +12,20 @@ for i in range(int(numBlocks)):
     line = infile.readline()
     blocks = line.rstrip()
     blocks = blocks.split(" ")
-    print(blocks)
     base_blocks.append([int(d) for d in blocks])
 
-print(base_blocks)
+blocks = []
+
+for block in base_blocks:
+    blocks.append(block)
+    blocks.append([block[2],block[1],block[0]])
+    blocks.append([block[0],block[2],block[1]])
+
+blocks.sort(key=lambda x: x[0]*x[1], reverse = 1)
+
+dp_table = [[] for block in blocks]
+dp_table[0] = blocks[0][2]
+for i in range(1, len(dp_table) - 1):
+    sublist = [dp_table[:i-1]]
+    if blocks[i][0] > blocks[i+1][0] and
+    dp_table[i] = dp_table[i-1] + newblockheight
