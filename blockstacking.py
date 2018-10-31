213521f1f3b7353d66b1d3f27353d58f3fc36a8f
jmrshl <jamesmrshl@gmail.com>
2018-10-30 20:39:32 -0700

    file parsing

--- blockstacking.py
+++ blockstacking.py
@@ -1,7 +1,18 @@
 #Joe Brennan and James Marshall
 #Block Stacking
 
-infile = open("infile.txt", "r")
-outfile = open("outfile.txt", "w")
+
+infile = open("blocks10.in", "r")
+#outfile = open("outfile.txt", "w")
 numBlocks = infile.readline()
 
+base_blocks = []
+
+for i in range(int(numBlocks)):
+    line = infile.readline()
+    blocks = line.rstrip()
+    blocks = blocks.split(" ")
+    print(blocks)
+    base_blocks.append([int(d) for d in blocks])
+
+print(base_blocks)
