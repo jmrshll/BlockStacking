d396a9ca753744d326a693aa736e8bc1f3f1d2d8
Joe Brennan <jwsbrennan@gmail.com>
2018-10-30 23:56:37 -0700

    updates

--- blockstacking.py
+++ blockstacking.py
@@ -1,6 +1,10 @@
 #Joe Brennan and James Marshall
 #Block Stacking
 from typing import Tuple
+import sys
+
+#infilePath = sys.argv[0]
+#outfilePath = sys.argv[1]
 
 infile = open("blocks10.in", "r")
 #outfile = open("outfile.txt", "w")
@@ -23,14 +27,31 @@ for block in base_blocks:
 
 blocks.sort(key=lambda x: x[0]*x[1], reverse = 1)
 
-list = [1,2,3]
-print(list[:1])
-
-dp_table = [Tuple[int,int] for block in blocks]
-dp_table[0] = blocks[0][2]
+dp_table = [Tuple[int,int,int] for block in blocks] #The tuples are (maxheight, index, previousblockindex)
+dp_table[0] = (blocks[0][2], 0, 0)
 for i in range(1, len(dp_table) - 1):
-    sublist = [dp_table[:i]]
-    sublist = filter(lambda x: (blocks[x[1]][0] < blocks[i][0] and blocks[x[1]][1] < blocks[i][1]) or (blocks[x[1]][0] < blocks[i][1] and blocks[x[1]][1] < blocks[i][0]), sublist)
-    dp_table[i] = blocks[i][2] + max(sublist,key=lambda x: x[0])
+    sublist = dp_table[:i]
+    sublist = filter((lambda x: ((blocks[x[1]][0] < blocks[i][0]) and (blocks[x[1]][1] < blocks[i][1])) 
+    			or ((blocks[x[1]][0] < blocks[i][1]) and (blocks[x[1]][1] < blocks[i][0]))), sublist)
+
+    if len(sublist) == 0:
+    	dp_table[i] = (blocks[i][2], i, -1)
+    else:
+		prevBlock = max(sublist, key=lambda x: x[0])
+		print("prevBlock = " + str(prevBlock))
+		newHeight = blocks[i][2] + prevBlock[0]
+		dp_table[i] = (newHeight, i, prevBlock[1])
 
 print(dp_table)
+
+maxHeightEntry = max(dp_table, key=lambda x: x[0])
+totalHeight = maxHeightEntry[0]
+solutionBlockList = []
+solutionBlockList.append(maxHeightEntry)
+currIndex = maxHeightEntry[1]
+while(dp_table[currIndex][2] != -1):
+	solutionBlockList.append(dp_table[dp_table[currIndex][2]])
+	currIndex = dp_table[currIndex][2]
+
+print("The tallest tower has " + str(len(solutionBlockList)) + " blocks and a height of " + str(totalHeight))
+
